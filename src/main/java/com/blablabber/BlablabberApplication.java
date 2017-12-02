d6102c112a12749fe7c728aafe85a9a307351362
sinisha.mihajlovski <sinisha.mihajlovski@domain.com>
2017-12-02 13:32:15 +0100

    Added project generated with spring initializr.

---
+++ src/main/java/com/blablabber/BlablabberApplication.java
@@ -0,0 +1,12 @@
+package com.blablabber;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlablabberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlablabberApplication.class, args);
+	}
+}
